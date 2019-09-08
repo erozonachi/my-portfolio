033f311f047d776dae3d20b46e31531364d9187b
erozonachi <erozonachi.james@gmail.com>
2019-09-08 22:20:43 +0100

    Uncomment pages and components dir in gitignore file

--- components/Header.js
+++ components/Header.js
@@ -25,6 +25,34 @@ const Header = () => (
         <a>Projects</a>
       </Link>
     </nav>
+    <style jsx>{`
+      div {
+        display: flex;
+        align-items: center;
+        position: absolute;
+        width: 1340px;
+        height: 61px;
+        top: 0px;
+        margin: 0 auto;
+      }
+      div > a {
+        display: flex;
+        align-items: center;
+        position: absolute;
+        width: 230px;
+        height: 61px;
+        left: 0px;
+        text-decoration: none;
+      }
+      div > nav {
+        display: flex;
+        justify-content: space-between;
+        position: absolute;
+        width: 730px;
+        height: 30px;
+        right: 0px;
+      }
+    `}</style>
   </div>
 );
 
